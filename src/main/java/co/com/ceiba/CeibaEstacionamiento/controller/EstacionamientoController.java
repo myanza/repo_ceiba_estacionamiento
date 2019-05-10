3a68cb14ddef469ee5e504269e3479537dd2dd3e
myanza <marcela.yanza28@gmail.com>
2019-05-10 17:31:17 -0500

    sdfsdf

--- src/main/java/co/com/ceiba/CeibaEstacionamiento/controller/EstacionamientoController.java
+++ src/main/java/co/com/ceiba/CeibaEstacionamiento/controller/EstacionamientoController.java
@@ -1,13 +1,19 @@
 package co.com.ceiba.CeibaEstacionamiento.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.com.ceiba.CeibaEstacionamiento.servicios.*;
+
 @RestController
+@RequestMapping("/bd_estacionamiento")
 @CrossOrigin(origins = "http://localhost:3306")
 
 public class EstacionamientoController 
 {
-
+	@Autowired
+	private ParqueaderoServicio parqueaderoSevicio;
 }

1f3af36d48f49906d547b9c3a6e719aacc6ea7a5
myanza <marcela.yanza28@gmail.com>
2019-05-15 15:50:21 -0500

    sdfsdfsdf

---
+++ src/test/java/co/com/ceiba/CeibaEstacionamiento/CeibaEstacionamientoApplicationTests.java
@@ -0,0 +1,30 @@
+package co.com.ceiba.CeibaEstacionamiento;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import co.com.ceiba.CeibaEstacionamiento.CeibaEstacionamientoApplication;
+import co.com.ceiba.CeibaEstacionamiento.dbconfiguracion.H2JpaConfig;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { CeibaEstacionamientoApplication.class, H2JpaConfig.class })
+public class CeibaEstacionamientoApplicationTests 
+{
+	@Test
+	public void whenValidName_thenEmployeeShouldBeFound()
+	{
+	    String name = "alex";   
+	  
+	     assertThat("alex")
+	      .isEqualTo(name);
+	 }
+}

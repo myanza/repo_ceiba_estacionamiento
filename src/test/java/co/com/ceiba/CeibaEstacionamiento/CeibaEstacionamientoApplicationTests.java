dc1c015f11b376ada558698810ee899210222525
myanza <marcela.yanza28@gmail.com>
2019-05-15 14:16:06 -0500

    sdfsdf

--- src/test/java/co/com/ceiba/CeibaEstacionamiento/CeibaEstacionamientoApplicationTests.java
+++ src/test/java/co/com/ceiba/CeibaEstacionamiento/CeibaEstacionamientoApplicationTests.java
@@ -12,11 +12,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CeibaEstacionamientoApplicationTests 
 {
-	/*@Test
-	public void whenValidName_thenEmployeeShouldBeFound() {
-	    String name = "alex";   
-	  
-	     assertThat("alex")
-	      .isEqualTo(name);
-	 }*/
+	
 }

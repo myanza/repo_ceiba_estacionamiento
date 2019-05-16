34758cc8c915646941df3655da40ecc95b13d93b
myanza <marcela.yanza28@gmail.com>
2019-05-16 10:16:40 -0500

    correccion nombnrad

--- src/main/java/co/com/ceiba/CeibaEstacionamiento/CeibaEstacionamientoApplication.java
+++ src/main/java/co/com/ceiba/CeibaEstacionamiento/CeibaEstacionamientoApplication.java
@@ -4,22 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages={"co.com.ceiba.ceibaestacionamiento.controller",
-		"co.com.ceiba.ceibaestacionamiento.dbconfiguracion",
-		"co.com.ceiba.ceibaestacionamiento.dominio",
-		"co.com.ceiba.ceibaestacionamiento.dominio.dto",
-		"co.com.ceiba.ceibaestacionamiento.persistencia.builders",
-		"co.com.ceiba.ceibaestacionamiento.persistencia.entidades",
-		"co.com.ceiba.ceibaestacionamiento.persistencia.repositorio",
-		"co.com.ceiba.ceibaestacionamiento.servicios",
-		"co.com.ceiba.ceibaestacionamiento.servicios.excepciones"})
+@ComponentScan(basePackages={"co.com.ceiba.CeibaEstacionamiento.controller",
+		"co.com.ceiba.CeibaEstacionamiento.dbconfiguracion",
+		"co.com.ceiba.CeibaEstacionamiento.dominio",
+		"co.com.ceiba.CeibaEstacionamiento.domini.dto",
+		"co.com.ceiba.CeibaEstacionamiento.persistencia.builders",
+		"co.com.ceiba.CeibaEstacionamiento.persistencia.entidades",
+		"co.com.ceiba.CeibaEstacionamiento.persistencia.repositorio",
+		"co.com.ceiba.CeibaEstacionamiento.servicios",
+		"co.com.ceiba.CeibaEstacionamiento.servicios.excepciones"})
 @SpringBootApplication
 public class CeibaestacionamientoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeibaestacionamientoApplication.class, args);
 	}
-	
-	
 
 }

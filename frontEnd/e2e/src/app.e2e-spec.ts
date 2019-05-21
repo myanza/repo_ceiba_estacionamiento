a7d35ab496f3b9af28577ea3d7aef7e2d75aa6c6
myanza <marcela.yanza28@gmail.com>
2019-05-21 17:44:42 -0500

    dfgdfgdf

--- frontEnd/e2e/src/app.e2e-spec.ts
+++ frontEnd/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,40 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
+import { By } from 'selenium-webdriver';
 
-describe('workspace-project App', () => {
+describe('workspace-project App', () =>
+{
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Debe mostrar el mensaje de bienvenida', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to frontEnd!');
+    expect(page.getTitleText()).toEqual('Bienvenido al Estacionamiento Ceiba');
+  });
+
+  it('Debe registrar un movil', () => {
+    page.navigateTo();
+    var tbxplaca = element(by.name('placaRegistro'));
+    element(By.id('CARRO')).click();
+    //var tbxcilindraje = element(by.name('cilindrajeRegistro'));
+
+    tbxplaca.sendKeys('FGH-123');
+
+    element(by.name('btnRegistrar')).click();
+
+    var tabla = element(by.tagName('mat-table'))
+    var cells = tabla.all(by.tagName('mat-cell'));
+
+    expect(cells.get(0).getText()).toEqual('FGH-123');
+    expect(cells.get(1).getText()).toEqual('CARRO');
+
+    /*element.all(by.repeater('factura in facturas')).then((posts) => {
+        var placa = posts[0].element(by.name('placa'));
+        expect(placa.getText()).toEqual('FGH-123');
+    });*/
   });
 
   afterEach(async () => {
